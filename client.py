68fc40f9cbc91bcc734b4f3b04355c873ef7a20e
p.gerasimenko <p.gerasimenko@medcall.center>
2026-02-26 20:39:45 +0300

    3

--- client.py
+++ client.py
@@ -1,7 +1,5 @@
 # client.py
 import aiohttp
-from aiohttp import payload_type
-
 from storage import save_config, load_config
 from config import CLIENT_ID, CLIENT_SECRET, DEBUG
 
@@ -33,7 +31,7 @@ async def refresh_token(auth: dict):
     if DEBUG:
         print("REFRESH TOKEN:", url, params)
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=params) as resp:
+        async with session.post(url, data=params) as resp:
             result = await resp.json()
     if "error" not in result:
         cfg = load_config()
